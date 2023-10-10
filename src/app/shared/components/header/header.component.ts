c72d61171451862eb9bbfbff6418fafeabb1aebf
FERNANDO OLMOS <fernando.olmos@afirma-solutions.com>
2023-10-09 18:43:38 -0600

    Feat: Cards mas bonitas

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -10,6 +10,22 @@ import { Router } from '@angular/router';
         <app-cart class="mouseHover" (click)="goToCheckout()"></app-cart>
       </span>
     </mat-toolbar>
+    <!-- <header>
+		<nav class="navegacion">
+			<ul class="menu">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Nosotros</a></li>
+				<li><a href="#">Servicios</a>
+					<ul class="submenu">
+						<li><a href="#">Servicio #1</a></li>
+						<li><a href="#">Servicio #2</a></li>
+						<li><a href="#">Servicio #3</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+	</header> -->
   `,
   styleUrls: ['./header.component.scss'],
 })
